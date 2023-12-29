950b80deb31f48204c3d0e33a0848c5f5e3c21c5
maiphuwowng77 <100115016+maiphuwowng77@users.noreply.github.com>
2023-12-29 14:51:45 +0700

    thong ke

--- src/components/ReportItem.js
+++ src/components/ReportItem.js
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Pie } from "react-chartjs-2";
 import { countOrdersByStatus } from "../firebase";
+import { TbTruckDelivery, TbMailCancel } from "react-icons/tb";
+import { IoMdCheckmark } from "react-icons/io";
+import { IoFileTrayFullOutline } from "react-icons/io5";
 
 const ReportItem = ({ user }) => {
   const [chartData, setChartData] = useState(null);
+  const [data, setData] = useState({
+    deliveringCount: 0, 
+    deliveredCount: 0, 
+    cancelledCount: 0, 
+    total: 0
+  });
 
   useEffect(() => {
     async function fetchData() {
       try {
         const { deliveringCount, deliveredCount, cancelledCount } = await countOrdersByStatus(user);
+        const data1 = {
+          deliveringCount: deliveringCount, 
+          deliveredCount: deliveredCount, 
+          cancelledCount: cancelledCount, 
+          total: deliveredCount + deliveringCount + cancelledCount
+        }
+        setData(data1);
         setChartData({
           labels: ["Đang vận chuyển", "Đã vận chuyển", "Đã bị huỷ"],
           datasets: [
@@ -29,10 +45,52 @@ const ReportItem = ({ user }) => {
   }, []);
 
   return (
-    <div className="container mx-auto mt-8">
-      <h2 className="text-3xl text-main-300 font-bold mb-8 flex items-center justify-center">Thống kê đơn hàng</h2>
-      <div className="bg-white rounded-lg shadow-md px-16 w-1/2 mb-8 mx-auto flex items-center justify-center">
-        {chartData ? <Pie data={chartData} className="p-16"/> : null}
+    <div className="flex flex-row">
+      <div className="w-3/4 container mt-8">
+        <h2 className="text-3xl text-main-300 font-bold mb-8 flex items-center justify-center">Thống kê đơn hàng</h2>
+        <div className="bg-white rounded-lg shadow-md px-16 w-3/4 h-[420px] mb-8 mx-auto flex items-center justify-center">
+          {chartData ? <Pie data={chartData} className="p-16"/> : null}
+        </div>
+      </div>
+      <div className="w-1/4 container mt-8 ml-[-50px] bg-white rounded-lg shadow-md flex">
+        <div className="ml-4 mt-5 p-2">
+          <div className="flex flex-row rounded-lg p-4 mb-5" style={{backgroundColor: "rgb(58,140,207)"}}>
+            <div>
+                <div className="rounded-full w-16 h-16 bg-[rgb(110,186,237)] flex items-center justify-center"><IoMdCheckmark  color="white" className="w-9 h-9"/></div>
+            </div>
+            <div className="flex flex-col pl-3 text-center pt-3 text-white">
+                <h5>Đã vận chuyển</h5>
+                <h4>{data.deliveredCount}</h4>
+            </div>
+          </div>
+          <div className="flex flex-row rounded-lg p-4 mb-5" style={{backgroundColor: "#f5a6b6"}}>
+            <div>
+                <div className="rounded-full w-16 h-16 bg-[#FFB6C1] flex items-center justify-center"><TbTruckDelivery color="white" className="w-10 h-10"/></div>
+            </div>
+            <div className="flex flex-col pl-3 text-center pt-3 text-white">
+                <h5>Đang vận chuyển</h5>
+                <h4>{data.deliveringCount}</h4>
+            </div>
+          </div>
+          <div className="flex flex-row rounded-lg p-4 mb-5" style={{backgroundColor: "rgb(247,185,84)"}}>
+            <div>
+                <div className="rounded-full w-16 h-16 bg-[rgb(247,220,154)] flex items-center justify-center"><TbMailCancel color="white" className="w-10 h-10"/></div>
+            </div>
+            <div className="flex flex-col pl-3 text-center pt-3 text-white">
+                <h5>Đã bị hủy</h5>
+                <h4 className="ml-1">{data.cancelledCount}</h4>
+            </div>
+          </div>
+          <div className="flex flex-row rounded-lg p-4 mb-5" style={{backgroundColor: "rgb(71,189,137)"}}>
+            <div>
+                <div className="rounded-full w-16 h-16 bg-[rgb(112,219,182)] flex items-center justify-center"><IoFileTrayFullOutline color="white" className="w-9 h-9"/></div>
+            </div>
+            <div className="flex flex-col pl-3 text-center pt-3 text-white">
+                <h5>Tổng đơn hàng</h5>
+                <h4>{data.total}</h4>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
